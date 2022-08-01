3b4991edbfaa57478c9fa835e8552540cbc0c458
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-08-01 06:29:43 +0200

    initializing state

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import SeasonDisplay from "./SeasonDisplay";
+// import SeasonDisplay from "./SeasonDisplay";
 
 const el = document.querySelector("#root");
 const root = ReactDOM.createRoot(el);
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null };
+  }
   render() {
     navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
