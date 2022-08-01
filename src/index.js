daca9cf2915a4b9ffb9c7ef4ba4e953de365c0af
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-08-01 14:14:55 +0200

    passing state prop

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import SeasonDisplay from "./SeasonDisplay";
+import SeasonDisplay from "./SeasonDisplay";
 
 const el = document.querySelector("#root");
 const root = ReactDOM.createRoot(el);
@@ -15,7 +15,7 @@ class App extends React.Component {
     );
   }
   render() {
-    if (this.state.lat) return <div>Latitude: {this.state.lat}</div>;
+    if (this.state.lat) return <SeasonDisplay lat={this.state.lat} />;
     if (this.state.errorMessage)
       return <div>Error: {this.state.errorMessage}</div>;
     return <div>Loading!</div>;
